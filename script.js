53b901a9e159ded8c27c395c14cc53461cfbc436
Danpalmeira92 <danpalmeira1992@gmail.com>
2024-10-28 18:35:18 -0300

    utilizando JQuery para selecionar elementos no html

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 $(document).ready(function(){
     console.log(document.querySelector('header btton'))
     console.log($('#botao-cancelar'))
+
+
+    $('header button').click(function() {
+        alert('Expandir formulário')
+    }) 
+
+    $('form').on('submit', function(e) {
+        console.log('submit')
+        e.preventDefault()
+    })
+
 })
