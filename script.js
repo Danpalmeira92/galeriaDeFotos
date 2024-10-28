727c1bf2f19b6014cb45dd0d9211075c116b98be
Danpalmeira92 <danpalmeira1992@gmail.com>
2024-10-28 18:42:48 -0300

    Manipulando o formulário com jQuery para que ele apreça e suma quando solicitado

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
-$(document).ready(function(){
+/* $(document).ready(function(){
     console.log(document.querySelector('header btton'))
-    console.log($('#botao-cancelar'))
-
+    console.log($('#botao-cancelar')) Teste para ver se o JQuery foi aplicado corretamente
+})
+    */
 
     $('header button').click(function() {
-        alert('Expandir formulário')
+        $('form').slideDown()
     }) 
+
+    $('#botao-cancelar').click(function() {
+        $('form').slideUp()
+    })
 
     $('form').on('submit', function(e) {
         console.log('submit')
         e.preventDefault()
     })
-
-})
